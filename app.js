86ceccf7c8e0575a7eee70bbb4519a4f8c8a64b3
Rifa-K-Malik <malikrif07@gmail.com>
2020-11-30 10:54:51 +0530

    Implementing: MongoDB

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/order');
 
+
+mongoose.connect('mongodb+srv://RifaKMalik:' + process.env.MONGO_ATLAS_PW + '@cluster0.acndi.mongodb.net/<dbname>?retryWrites=true&w=majority' , {
+    useMongoClient: true
+});
 
 app.use('/products', productRoutes);
 app.use('/order', orderRoutes);
